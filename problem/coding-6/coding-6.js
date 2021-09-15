6b8e864da81742c9ec4bcc39fb94ad673d9d6673
Jo Jae Hoon <dev.hoony@gmail.com>
2021-09-15 21:54:35 +0900

    LeetCode-4 (다른 답안 추가)

--- problem/coding-6/coding-6.js
+++ problem/coding-6/coding-6.js
@@ -1,4 +1,4 @@
-const solution = (nums1, nums2) => {
+const solution1 = (nums1, nums2) => {
   const concatArr = nums1.concat(nums2);
   concatArr.sort((a, b) => a - b);
 
@@ -14,4 +14,34 @@ const solution = (nums1, nums2) => {
   return result;
 };
 
-module.exports = { solution };
+const solution2 = (nums1, nums2) => {
+  const length = nums1.length + nums2.length;
+  const midIndex = Math.floor(length / 2);
+  let newArr = [];
+
+  let i1 = 0;
+  let i2 = 0;
+  for (let i = 0; i <= midIndex; i++) {
+    if (i1 < nums1.length && i2 < nums2.length) {
+      if (nums1[i1] < nums2[i2]) {
+        newArr[i] = nums1[i1];
+        i1++;
+      } else {
+        newArr[i] = nums2[i2];
+        i2++;
+      }
+    } else if (i1 === nums1.length) {
+      newArr[i] = nums2[i2];
+      i2++;
+    } else {
+      newArr[i] = nums1[i1];
+      i1++;
+    }
+  }
+
+  return length % 2
+    ? newArr[midIndex]
+    : (newArr[midIndex] + newArr[midIndex - 1]) / 2;
+};
+
+module.exports = { solution1, solution2 };
