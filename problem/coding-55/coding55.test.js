07b300670568f7c9f504bcca9a9b75241466c5b8
Jo Jae Hoon <dev.hoony@gmail.com>
2023-12-02 02:05:55 +0900

    2 x n 타일링

---
+++ problem/coding-55/coding55.test.js
@@ -0,0 +1,7 @@
+const { solution } = require("./coding55.js");
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12900
+
+test("coding-55", () => {
+  expect(solution(4)).toBe(5);
+});
